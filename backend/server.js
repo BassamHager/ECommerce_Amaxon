8510cc8452f3dcf12e5545f062aa20cc2e086af7
BassamHajar <b2dndr1@gmail.com>
2020-10-08 18:52:58 +0200

    minor updates on backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ import data from "./data";
 const app = express();
 const PORT = 5000;
 
-app.length("/api/products", (req, res) => {
+app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
 app.listen(PORT, () =>
-  console.log(`Server started at http://localhost:${PORT}`)
+  console.log(`Server started at http://localhost:${PORT}/api/products`)
 );
