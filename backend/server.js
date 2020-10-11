dfb43864c8ae68c92193805abfc63fffbf6e181b
BassamHajar <b2dndr1@gmail.com>
2020-10-11 14:43:37 +0200

    added productDetails and applied in Redux as well

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ import data from "./data";
 const app = express();
 const PORT = 5000;
 
+// product 
+app.get("/api/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const product = data.products.find(x=>x._id === productId)
+  if (product) res.send(product)
+  else res.status(404).send({ msg:'Product not found!' })
+});
+
+// products
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
